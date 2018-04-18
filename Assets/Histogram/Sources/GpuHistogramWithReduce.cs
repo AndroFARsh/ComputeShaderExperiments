66651e7b739abcc7827f042007aab42ea2824fe8
Anton.Kukhlevskyi <anton.kukhlevskyi@lookout.com>
2018-04-18 10:05:09 +0300

    GPU histogram with reduce

--- Assets/Histogram/Sources/GpuHistogramWithReduce.cs
+++ Assets/Histogram/Sources/GpuHistogramWithReduce.cs
@@ -6,7 +6,6 @@ public class GpuHistogramWithReduce : IHistogram
 {
     private const string KERNEL_NAME_HISTO      = "CSHistogram";
     private const string KERNEL_NAME_REDUCE     = "CSReduce";
-    private const string KERNEL_NAME_DEFRAG     = "CSDefragmentation";
     
     private const string PARAM_GRID             = "g_gridDimen";
     private const string PARAM_BUFF_IN          = "g_in";
@@ -18,7 +17,6 @@ public class GpuHistogramWithReduce : IHistogram
     private readonly ComputeShader shader;
     private readonly int kernelIdHisto;
     private readonly int kernelIdReduce;
-    private readonly int kernelIdDefrag;
     private readonly int totalThreadsInBlock;
     
     public GpuHistogramWithReduce(ComputeShader s)
@@ -26,7 +24,6 @@ public class GpuHistogramWithReduce : IHistogram
         shader = s;
         kernelIdHisto  = shader.FindKernel(KERNEL_NAME_HISTO);
         kernelIdReduce = shader.FindKernel(KERNEL_NAME_REDUCE);
-        kernelIdDefrag = shader.FindKernel(KERNEL_NAME_DEFRAG);
         totalThreadsInBlock = shader.TotalThreadsInBlock(kernelIdHisto);
     }
 
@@ -35,85 +32,35 @@ public class GpuHistogramWithReduce : IHistogram
         if (bins > totalThreadsInBlock) throw new ArgumentException("histo base shouldn't be larger than: "+totalThreadsInBlock);
         
         var grid = Utils.TrimToBlock(data.Length, totalThreadsInBlock);
+        var sizePowerOfTwo  = Utils.NextPowerOfTwo(grid);
         
         var dataBuffer = new ComputeBuffer(data.Length, sizeof(int));
-        var histoBuffer = new ComputeBuffer(bins * grid, sizeof(int));
-        var testBuffer = new ComputeBuffer(bins * grid, sizeof(int));
+        var histoBuffer = new ComputeBuffer(bins * sizePowerOfTwo, sizeof(int));
         
         dataBuffer.SetData(data);
 
         shader.SetInt(PARAM_OFFSET, 0);
         shader.SetInt(PARAM_SIZE, data.Length);
         shader.SetInt(PARAM_BIN_SIZE, bins);
-        shader.SetInts(PARAM_GRID, grid, 1, 1);
+        shader.SetInts(PARAM_GRID, sizePowerOfTwo, 1, 1);
 
         shader.SetBuffer(kernelIdHisto, PARAM_BUFF_IN,  dataBuffer);
         shader.SetBuffer(kernelIdHisto, PARAM_BUFF_OUT, histoBuffer);
-        shader.Dispatch(kernelIdHisto, grid, 1, 1);
-
-        {
-            var tmp = new int[grid * bins];
-            histoBuffer.GetData(tmp);
-            LogUtils.PrintArray(tmp, "histo {"+grid+"}");
-
-            var indx = -1;
-            var t = new int[bins];
-            for (var i = 0; i < tmp.Length; ++i)
-            {
-                if (i % grid == 0) ++indx;
-                t[indx] += tmp[i];
-            }
-            
-            LogUtils.PrintArray(t, "histo {"+grid+"}");
-        }
+        shader.Dispatch(kernelIdHisto, sizePowerOfTwo, 1, 1);
 
         if (grid > 1)
         {
             shader.SetBuffer(kernelIdReduce, PARAM_BUFF_IN,  histoBuffer);
             shader.SetBuffer(kernelIdReduce, PARAM_BUFF_OUT, histoBuffer);
-            shader.SetBuffer(kernelIdReduce, "g_test", testBuffer);
-
-            var sizePowerOfTwo  = Utils.NextPowerOfTwo(grid);
             for (var step = 1; step < sizePowerOfTwo; step *= totalThreadsInBlock)
             {
                 var size = Utils.TrimToBlock(sizePowerOfTwo, step);
                 var gridTotal = Utils.TrimToBlock(size, totalThreadsInBlock);
-
-                shader.SetInt(PARAM_SIZE, Math.Min(size, grid));
-                shader.SetInt(PARAM_OFFSET, grid);
-                shader.SetInt(PARAM_BIN_SIZE, bins);
+                
+                shader.SetInt(PARAM_SIZE, size);
+                shader.SetInt(PARAM_OFFSET, Math.Min(size, totalThreadsInBlock));
                 shader.SetInts(PARAM_GRID, gridTotal, bins, 1);
                 shader.Dispatch(kernelIdReduce, gridTotal, bins, 1);
-                
-                {
-                    var tmp = new int[grid * bins];
-                    histoBuffer.GetData(tmp);
-                    LogUtils.PrintArray(tmp, "gpu histo {"+step+"|"+ Math.Min(size, grid)+"|"+totalThreadsInBlock+"}");
-//                    
-//                    var indx = -1;
-//                    for (var i = 0; i < tmp.Length; ++i)
-//                    {
-//                        if (i % (Math.Min(size, grid) / gridTotal) == 0) indx = i;
-//                        if (indx != i) tmp[indx] += tmp[i];
-//                    }
-//                    LogUtils.PrintArray(tmp, "cpu histo {"+step+"/"+size+"}");
-                    
-                    testBuffer.GetData(tmp);
-                    LogUtils.PrintArray(tmp, "gpu test {"+step+"|"+ Math.Min(size, grid)+"|"+totalThreadsInBlock+"}");
-                    
-                }
-            }
-
-            {
-                shader.SetBuffer(kernelIdDefrag, PARAM_BUFF_IN,  histoBuffer);
-                shader.SetBuffer(kernelIdDefrag, PARAM_BUFF_OUT, histoBuffer);
-                
-                var gridTotal = Utils.TrimToBlock(bins, totalThreadsInBlock);
-                shader.SetInt(PARAM_SIZE, bins * grid);
-                shader.SetInt(PARAM_OFFSET, grid);   
-                shader.SetInt(PARAM_BIN_SIZE, bins);
-                shader.SetInts(PARAM_GRID, gridTotal, 1, 1);
-                shader.Dispatch(kernelIdDefrag, gridTotal, 1, 1);
             }
         }
 
@@ -121,7 +68,6 @@ public class GpuHistogramWithReduce : IHistogram
         histoBuffer.GetData(result);
         histoBuffer.Release();
         dataBuffer.Release();
-        testBuffer.Release();
         return result;
     }
 }
